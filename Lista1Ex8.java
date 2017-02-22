bf78f5a73f42d8c75a733b238f08f59f149706d7
Joao Wind <jotahws.20@hotmail.com>
2017-02-22 14:22:32 -0300

    teste de arquivo

---
+++ Lista1Ex8.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.*;
+
+class Lista1Ex8
+{
+	public static void main (String[] args)
+	{
+		Scanner scn = new Scanner(System.in);
+		System.out.print("Digite nome e sobrenome: ");
+		String nome = scn.next();
+		String sobrenome = scn.next();
+		System.out.println(sobrenome);
+                System.out.println(nome);
+
+	}
+}
