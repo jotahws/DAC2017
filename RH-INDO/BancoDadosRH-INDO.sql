aeb999bb32f58f2eac4459c2b58ad1844a533955
araujoito <araujoito@gmail.com>
2017-04-18 21:04:24 -0300

    Banco de Dados Atoa/ Rh-indo
    Revisar

---
+++ RH-INDO/BancoDadosRH-INDO.sql
@@ -0,0 +1,59 @@
+create table Funcionario(
+	id int not null auto_increment,
+    idCargo int,
+    idDepartamento int,
+    idEndereco int,
+    nome varchar(100) not null,
+    cpf varchar(11) unique,
+    rg varchar(11) unique,
+    email varchar(40),
+    celular varchar(14),
+    primary key(id),
+    foreign key(idCargo) references Cargo(id),
+    foreign key(idDepartamento) references Departamento(id),
+    foreign key(idEndereco) references Endereco(id)
+);
+
+create table Cargo(
+	id int not null auto_increment,
+    nome varchar(50) not null,
+    salario double,
+    requisitos varchar(100),
+    horasMinimas time,
+    descontoImposto int,
+    primary key(id)
+);
+
+
+create table Departamento(
+	id int not null auto_increment,
+    nome varchar(50) not null,
+    localizacao varchar(50) not null,
+    primary key(id)
+);
+
+
+create table Endereco(
+	id int not null auto_increment,
+    idCidade int,
+    rua varchar(100),
+    numero varchar(5),
+    cep varchar(9),
+    primary key(id),
+    foreign key(idCidade) references Cidade(id)
+);
+
+create table Cidade(
+	id int not null auto_increment,
+    idEstado int,
+    nome varchar(100) not null,
+    primary key(id),
+    foreign key(idEstado) references Estado(id)
+);
+
+create table Estado(
+	id int not null auto_increment,
+    sigla varchar(2),
+    nome varchar(50),
+    primary key(id)
+);
