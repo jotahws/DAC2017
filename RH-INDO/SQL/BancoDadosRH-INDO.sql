80bf0f6d14fbc29fa8bcb1605ceab4f59dfb3a77
araujoito <araujoito@gmail.com>
2017-05-17 20:11:23 -0300

    SQL RINDO ajeitado

--- RH-INDO/SQL/BancoDadosRH-INDO.sql
+++ RH-INDO/SQL/BancoDadosRH-INDO.sql
@@ -1,18 +1,4 @@
-create table Funcionario(
-	id int not null auto_increment,
-    idCargo int,
-    idDepartamento int,
-    idEndereco int,
-    nome varchar(100) not null,
-    cpf varchar(11) unique,
-    rg varchar(11) unique,
-    email varchar(40),
-    celular varchar(14),
-    primary key(id),
-    foreign key(idCargo) references Cargo(id),
-    foreign key(idDepartamento) references Departamento(id),
-    foreign key(idEndereco) references Endereco(id)
-);
+
 
 create table Cargo(
 	id int not null auto_increment,
@@ -59,10 +45,28 @@ create table Estado(
     primary key(id)
 );
 
+create table Funcionario(
+	id int not null auto_increment,
+    idCargo int,
+    idDepartamento int,
+    idEndereco int,
+    nome varchar(100) not null,
+    cpf varchar(11) unique,
+    rg varchar(11) unique,
+    email varchar(40),
+    celular varchar(14),
+    primary key(id),
+    foreign key(idCargo) references Cargo(id),
+    foreign key(idDepartamento) references Departamento(id),
+    foreign key(idEndereco) references Endereco(id)
+);
+
+
 ALTER TABLE `sistema_rhindo`.`Cargo` 
 CHANGE COLUMN `horasMinimas` `horasMinimas` INT NULL DEFAULT NULL ,
 CHANGE COLUMN `descontoImposto` `descontoImposto` DOUBLE NULL DEFAULT NULL ,
-CHANGE COLUMN `requisitos` `requisitos` VARCHAR(1000) NULL DEFAULT NULL ,
+CHANGE COLUMN `requisitos` `requisitos` VARCHAR(1000) NULL DEFAULT NULL;
+ALTER TABLE `sistema_rhindo`.`Funcionario`
 ADD COLUMN `perfil` VARCHAR(45) NULL AFTER `celular`;
 
 ALTER TABLE `sistema_rhindo`.`Endereco` 
