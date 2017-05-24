1afcf2667807505fe693b4ac5c9abc48f0a217c5
araujoito <araujoito@gmail.com>
2017-05-23 21:42:13 -0300

    Função criptografa a senha

--- RH-INDO/SistemaRHINDO/src/java/beans/Funcionario.java
+++ RH-INDO/SistemaRHINDO/src/java/beans/Funcionario.java
@@ -6,6 +6,9 @@
 package beans;
 
 import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  *
@@ -143,4 +146,18 @@ public class Funcionario implements Serializable {
         this.senha = senha;
     }
 
+    public String criptografa(String senha) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        
+        MessageDigest algorithm = MessageDigest.getInstance("SHA-256");
+        byte messageDigest[] = algorithm.digest(senha.getBytes("UTF-8"));
+
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : messageDigest) {
+            hexString.append(String.format("%02X", 0xFF & b));
+        }
+        senha = hexString.toString();
+        
+        return senha;
+    }
+    
 }
